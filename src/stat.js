e8e69299867390a1b122e938427f80a8dce8a0d0
Shim Heungwoon <ajaxlab7@gmail.com>
2019-03-25 02:03:04 +0900

    add fs.stat case

--- src/stat.js
+++ src/stat.js
@@ -8,16 +8,43 @@ fstat takes a file descriptor rather than a path.
 
 const fs = require('fs');
 
-fs.lstat('./real', (e, stats) => {
+fs.stat('./real', (e, stat) => {
   if (e) {
     console.info(e);
   }
-  console.info('stats', stats);
+  // console.info('/real stat', stat);
+  console.info('/real stat.isFile()', stat.isFile());
+  console.info('/real stat.isSymbolicLink()', stat.isSymbolicLink());
+  console.info('/real stat.isDirectory()', stat.isDirectory());
 });
 
-fs.lstat('./sym', (e, stats) => {
+fs.lstat('./real', (e, lstat) => {
   if (e) {
     console.info(e);
   }
-  console.info('stats', stats);
+  // console.info('/real lstat', lstat);
+  console.info('/real lstat.isFile()', lstat.isFile());
+  console.info('/real lstat.isSymbolicLink()', lstat.isSymbolicLink());
+  console.info('/real lstat.isDirectory()', lstat.isDirectory());
+});
+
+fs.stat('./sym', (e, stat) => {
+  if (e) {
+    console.info(e);
+  }
+  // console.info('/sym stats', stat);
+  console.info('/sym stats.isFile()', stat.isFile());
+  console.info('/sym stats.isSymbolicLink()', stat.isSymbolicLink());
+  console.info('/sym stats.isDirectory()', stat.isDirectory());
+});
+
+
+fs.lstat('./sym', (e, lstat) => {
+  if (e) {
+    console.info(e);
+  }
+  // console.info('/sym lstat', lstat);
+  console.info('/sym lstat.isFile()', lstat.isFile());
+  console.info('/sym lstat.isSymbolicLink()', lstat.isSymbolicLink());
+  console.info('/sym lstat.isDirectory()', lstat.isDirectory());
 });
